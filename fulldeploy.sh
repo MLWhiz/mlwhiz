86611598da13f4a5e537eb81952af4967fb89979
Rahul Agarwal <ragarwal@rokue.com>
2022-12-06 01:49:26 +0000

    changed deployfile

--- fulldeploy.sh
+++ fulldeploy.sh
@@ -1,11 +1,13 @@
 rsync -avu --delete "static/images/" "assets/images"
 hugo --minify
 cd public
+git pull origin master
 echo "mlwhiz.com" >CNAME
 git add .
 git commit -m "msg"
 git push origin master
 cd ..
+git pull origin master
 git add .
 git commit -m "msg"
 git push origin master
